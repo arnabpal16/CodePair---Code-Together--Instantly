87bf705d4bad51a39e8e0932e99d7f4c3c4f1a37
Arnab Pal <arnab.pal.21@aot.edu.in>
2025-12-25 12:21:13 +0530

    vite config js edit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   server: {
-//     proxy: {
-//       '/execute': 'http://localhost:3001',
-//     }
-//   }
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -32,7 +18,7 @@ export default defineConfig({
   },
 
   server: {
-    
+
     allowedHosts: [
       'ebwgh-103-188-163-178.a.free.pinggy.link',
     ],
